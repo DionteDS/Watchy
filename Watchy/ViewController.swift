6f9758a3e5a5ddd8d2dbe07b43131789ade7c688
Dionte <Dionte_sb@yahoo.com>
2020-03-19 10:38:40 -0700

    Initial commit

---
+++ Watchy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Watchy
+//
+//  Created by Dionte Silmon on 3/19/20.
+//  Copyright © 2020 Dionte Silmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
