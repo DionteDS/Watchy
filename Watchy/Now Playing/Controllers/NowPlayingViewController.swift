2f6293599d55af9abf5a2b565cd8fef346d7d720
Dionte <Dionte_sb@yahoo.com>
2020-03-19 11:05:42 -0700

    Create a tab bar controller and also created a view controller for now playing movies

--- Watchy/Now Playing/Controllers/NowPlayingViewController.swift
+++ Watchy/Now Playing/Controllers/NowPlayingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NowPlayingViewController.swift
 //  Watchy
 //
 //  Created by Dionte Silmon on 3/19/20.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NowPlayingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
-
 }
-
